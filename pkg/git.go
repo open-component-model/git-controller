6059c0c87458ee588cabd32f0e4fd1696aa2af77
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-10-18 11:02:50 +0200

    Added options for pushing.

--- pkg/git.go
+++ pkg/git.go
@@ -2,7 +2,38 @@ package pkg
 
 import "context"
 
+// BasicAuth provides information for basic authentication. The expected format is Username as username and
+// Password is usually a token.
+type BasicAuth struct {
+	Username string
+	Password string
+}
+
+// SSH provides authentication for private repositories using an SSH key. The key has to be the Private key.
+type SSH struct {
+	PemBytes []byte
+	User     string
+	Password string
+}
+
+// Auth defines authentication options for repositories.
+type Auth struct {
+	BasicAuth *BasicAuth
+	SSH       *SSH
+}
+
+// PushOptions contains settings for a push action.
+type PushOptions struct {
+	Auth             *Auth
+	URL              string
+	Message          string
+	Name             string
+	Email            string
+	SnapshotLocation string
+	Ref              string
+}
+
 // Git defines an interface to abstract git operations.
 type Git interface {
-	Push(ctx context.Context, msg, location, destination string) error
+	Push(ctx context.Context, opts *PushOptions) error
 }
--- pkg/providers/gogit/git.go
+++ pkg/providers/gogit/git.go
@@ -12,50 +12,66 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/cache"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 	"github.com/go-git/go-git/v5/storage/filesystem"
 	"github.com/go-logr/logr"
+
+	"github.com/open-component-model/git-sync-controller/pkg"
 )
 
 type Git struct {
 	Logger logr.Logger
 }
 
-func NewGit(log logr.Logger) *Git {
+func NewGoGit(log logr.Logger) *Git {
 	return &Git{
 		Logger: log,
 	}
 }
 
-func (g *Git) Push(ctx context.Context, msg, snapshotLocation, destination string) error {
-	g.Logger.V(4).Info("running push operation", "msg", msg, "snapshot", snapshotLocation, "destination", destination)
+func (g *Git) Push(ctx context.Context, opts *pkg.PushOptions) error {
+	g.Logger.V(4).Info("running push operation", "msg", opts.Message, "snapshot", opts.SnapshotLocation, "url", opts.URL)
 	// Get the snapshot from snapshotLocation
 	// move to this tmp folder or ( Fetch ) to this tmp folder once the git remote is initialised?
 	dir, err := os.MkdirTemp("", "clone")
 	if err != nil {
 		return fmt.Errorf("failed to initialise temp folder: %w", err)
 	}
+
 	fs := osfs.New(dir)
 	r, err := git.Init(filesystem.NewStorage(fs, cache.NewObjectLRUDefault()), nil)
 	if err != nil {
 		return fmt.Errorf("failed to initialise git repo: %w", err)
 	}
+
 	if _, err = r.CreateRemote(&config.RemoteConfig{
 		Name: "origin",
-		URLs: []string{destination},
+		URLs: []string{opts.URL},
 	}); err != nil {
 		return fmt.Errorf("failed to create remote: %w", err)
 	}
-	// TODO: Figure out if this is needed at all.
-	// Probably yes, since we are pushing into a gitops repository.
+
 	fetchOptions := &git.FetchOptions{
-		RefSpecs: []config.RefSpec{config.RefSpec("refs/heads/main")},
+		RefSpecs: []config.RefSpec{config.RefSpec(opts.Ref)},
 		Depth:    1,
 	}
-	// Detect what kind of Auth is provided.
-	fetchOptions.Auth = &http.BasicAuth{
-		Username: "Skarlso",
-		Password: "token",
+
+	if opts.Auth != nil {
+		if v := opts.Auth.BasicAuth; v != nil {
+			fetchOptions.Auth = &http.BasicAuth{
+				Username: v.Username,
+				Password: v.Password,
+			}
+		}
+		if v := opts.Auth.SSH; v != nil {
+			pb, err := ssh.NewPublicKeys(v.User, v.PemBytes, v.Password)
+			if err != nil {
+				return fmt.Errorf("failed to create public key authentication: %w", err)
+			}
+			fetchOptions.Auth = pb
+		}
 	}
+
 	if err := r.Fetch(fetchOptions); err != nil {
 		return fmt.Errorf("failed to fetch remote ref '%s': %w", "main", err)
 	}
@@ -70,14 +86,14 @@ func (g *Git) Push(ctx context.Context, msg, snapshotLocation, destination strin
 	if err := w.AddGlob("."); err != nil {
 		return fmt.Errorf("failed to add items to worktree: %w", err)
 	}
-	opts := git.CommitOptions{
+	commitOpts := &git.CommitOptions{
 		Author: &object.Signature{
-			Name:  "Gergely Brautigam",
-			Email: "182850+Skarlso@users.noreply.github.com",
+			Name:  opts.Name,
+			Email: opts.Email,
 			When:  time.Now(),
 		},
 	}
-	commit, err := w.Commit("Uploading snapshot to location", &opts)
+	commit, err := w.Commit("Uploading snapshot to location", commitOpts)
 	if err != nil {
 		return fmt.Errorf("failed to commit changes: %w", err)
 	}
